f5baed44c204daa6abfe60f82a3a6084b2d4379a
ynoh <nyg9311@gmail.com>
2023-07-05 12:27:26 +0900

    Add textarea

--- main.py
+++ main.py
@@ -12,10 +12,17 @@ menu_1.add_command(label="새파일")
 menu_1.add_command(label="저장")
 menu_1.add_separator()
 menu_1.add_command(label="종료", command=window.destroy)
+menu.add_cascade(label="파일", menu=menu_1)
 
 menu_2 = Menu(menu, tearoff=0)
 menu_2.add_command(label="만든이")
 menu.add_cascade(label="만든이", menu=menu_2)
 
+text_area = Text(window)
+window.grid_rowconfigure(0, weight=1)
+window.grid_columnconfigure(0, weight=1)
+text_area.grid(sticky = N + E + S + W)
+
 window.config(menu=menu)
+
 window.mainloop()
