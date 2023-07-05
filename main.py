dbf59e1ce2b29e1247a021b89891c1e499453ae2
ynoh <nyg9311@gmail.com>
2023-07-05 12:39:56 +0900

    button 활성화!

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 from tkinter import *
 from tkinter.filedialog import *
 
+def new_file():
+    text_area.delete(1.0,END)
+
+def save_file():
+    f = asksaveasfile(mode = "w", defaultextension=".txt", filetypes=[('Text files', '.txt')])
+    text_save = str(text_area.get(1.0, END))
+    f.write(text_save)
+    f.close()
+
+def marker():
+    help_view = Toplevel(window)
+    help_view.geometry("300x50")
+    help_view.title("만든이")
+    lb = Label(help_view, text = "노윤기가 만든 메모장입니다.")
+    lb.pack()
+
+
 window = Tk()
 window.title("Notepad")
 window.geometry("400x400")
@@ -8,14 +25,14 @@ window.resizable(False, False)
 
 menu = Menu(window)
 menu_1 = Menu(menu, tearoff=0)
-menu_1.add_command(label="새파일")
-menu_1.add_command(label="저장")
+menu_1.add_command(label="새파일", command=new_file)
+menu_1.add_command(label="저장", command=save_file)
 menu_1.add_separator()
 menu_1.add_command(label="종료", command=window.destroy)
 menu.add_cascade(label="파일", menu=menu_1)
 
 menu_2 = Menu(menu, tearoff=0)
-menu_2.add_command(label="만든이")
+menu_2.add_command(label="만든이", command=marker)
 menu.add_cascade(label="만든이", menu=menu_2)
 
 text_area = Text(window)
